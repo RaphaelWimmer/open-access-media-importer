b6bf54d6c60e8c237586bd6ea489dbe1dbbd63c7
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-01-17 01:37:49 +0800

    + PubMed XML archive download with progress bar

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, sys, tarfile
+import os, progressbar, sys, tarfile
 
 from ftplib import FTP
 from xml.etree.ElementTree import ElementTree
 
-DIRECTORY = 'PubMed'
+FTP_SERVER = 'ftp.ncbi.nlm.nih.gov'
+CACHE_DIRECTORY = 'PubMed'
 
 def get_PubMed_XML_TAR_GZ():
     """
     This function downloads XML archive files from PubMed.
     """
-    SERVER = 'ftp.ncbi.nlm.nih.gov'
 
-    ftp = FTP(SERVER)
+    ftp = FTP(FTP_SERVER)
     ftp.login()
 
-    filenames = {
-        'pub/pmc/articles.A-B.tar.gz': 'articles.A-B.tar.gz',
-        'pub/pmc/articles.C-H.tar.gz': 'articles.C-H.tar.gz',
-        'pub/pmc/articles.I-N.tar.gz': 'articles.I-N.tar.gz',
-        'pub/pmc/articles.O-Z.tar.gz': 'articles.O-Z.tar.gz'
-    }
+    ftp_filenames = (
+        'pub/pmc/articles.A-B.tar.gz',
+        'pub/pmc/articles.C-H.tar.gz',
+        'pub/pmc/articles.I-N.tar.gz',
+        'pub/pmc/articles.O-Z.tar.gz'
+    )
 
-    for remote_name, local_name in filenames.iteritems():
-        sys.stderr.write("Downloading %s from %s, saving as %s …" % (
-            remote_name, SERVER, local_name)
-        )
-        ftp.retrbinary(
-            "RETR %s" % remote_name,
-            open(
-                os.path.join(DIRECTORY, local_name), 'wb'
-            ).write
-        )
-        sys.stderr.write('done.\n')
+    for remote_filename in ftp_filenames:
+        local_filename = os.path.join(CACHE_DIRECTORY,
+            os.path.split(remote_filename)[-1])
+
+        ftp.sendcmd('TYPE i')  # switch to binary mode,
+        remote_filesize = ftp.size(remote_filename)
+        try:
+            local_filesize = os.stat(local_filename).st_size
+        except OSError:  # File does not exist
+            local_filesize = 0
+
+        if remote_filesize == local_filesize:
+            sys.stderr.write("%s is up to date, skipping.\n" % local_filename)
+        else:
+            sys.stderr.write("Downloading %s from %s, saving as %s …\n" % \
+                (remote_filename, FTP_SERVER, local_filename))
+
+            with open(local_filename, 'wb') as f:
+                p = progressbar.ProgressBar(maxval=remote_filesize)
+
+                def callback(chunk):
+                    f.write(chunk)
+                    p.update(p.currval + len(chunk))
+
+                ftp.retrbinary("RETR %s" % remote_filename, callback)
 
     ftp.quit()
 
+get_PubMed_XML_TAR_GZ()
 
 def find_PubMed_articles_with_supplementary_materials(filename):
     """
