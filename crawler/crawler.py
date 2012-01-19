ac5be8fb0e8bc6ddb0ba9be71d48dee51c4097fa
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2012-01-19 09:39:25 +0100

    added GPL3 license and short description

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#    Open Access Media Importer
+#    crawler.py - download supplementary materials of open access articles
+#
+#    Copyright (C) 2012  Nils Dagson Moskopp <nils@dieweltistgarnichtso.net>
+#    Copyright (C) 2012  Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, progressbar, sys, tarfile
 
 from ftplib import FTP
